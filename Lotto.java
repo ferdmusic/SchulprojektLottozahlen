33c61719ab424f7f0366cf37e95570b695153d54
Ferd-fb <80982592+Ferd-fb@users.noreply.github.com>
2023-03-22 15:19:35 +0100

    https://github.com/Ferd-fb/SchulprojektLottozahlen

--- Lotto.java
+++ Lotto.java
@@ -69,6 +69,7 @@ public class Lotto
                 }
                 border[0] = zahlensammlung[z+1];//linker rand des Verschiebens ist z+1
                 border[1] = zahlensammlung[i];//rechter rand ist i
+                String zwischenspeicher = zahlensammlung[z];
                 
                 wv = border[1] - border[2]; //wie viele Stellen braucht der zwischenarray, der nun deklariert wird
                 int[] verschiebearr = new verschiebearr[wv];
